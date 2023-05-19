8ad861235cab174a1cf1a89cc596f49a4f9f9530
Prashanthg01 <prashanthg9845@gmail.com>
2023-05-19 14:25:56 +0530

    commit

---
+++ projbackend/projbackend/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from appbackend.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', ReactView.as_view(), name="xxx"),
+]
